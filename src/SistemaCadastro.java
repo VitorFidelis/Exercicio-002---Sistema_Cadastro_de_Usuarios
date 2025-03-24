e06ebe6b8a20bc5135ccbc3cad41796929722022
Vitor Fidelis <109974299+VitorFidelis@users.noreply.github.com>
2025-03-24 10:30:22 -0300

    Update SistemaCadastro.java

--- src/SistemaCadastro.java
+++ src/SistemaCadastro.java
@@ -6,10 +6,10 @@ public class SistemaCadastro{
 //	Atributo do tipo lista com modificador de visibilidade private, onde so pode ser visto dentro dessa classe
 	private List<Usuario> usuarios = new ArrayList<>();
 	
-//	metodo de cadastro onde em sua assinatura é informa que esse metodo pode conter uma exceção, informamos isso atraves do uso do throws
+//	metodo de cadastro onde em sua assinatura é informado que esse metodo pode conter uma exceção, informamos isso atraves do uso do throws
 //	sendo necessario informar na assinatura do metodo, pois se trata de uma exceção checked
 	public void cadastrarUsuario(Usuario usuario) throws CadastroException {
-//		realizado uma validação que verifica se a idade do usuario é menor que 18
+//		realizando validação que verifica se a idade do usuario é menor que 18
 		if (usuario.getIdade() < 18) {
 //			se a idade do usuario for menor que 18, é lançado uma axceção atraves do uso do throw e ao mesmo tempo é instanciado
 //			o objeto do tipo exception e recebendo a mensagem no contrutor.
