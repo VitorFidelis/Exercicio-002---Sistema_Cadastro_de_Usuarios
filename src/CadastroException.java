cef499932e51cd311b2bf5bb5410da4bd763e08b
Vitor Fidelis <109974299+VitorFidelis@users.noreply.github.com>
2025-03-24 10:26:23 -0300

    Update CadastroException.java

--- src/CadastroException.java
+++ src/CadastroException.java
@@ -1,5 +1,5 @@
-//classe que contem o nome da minha exceção personalizada, extendendo a classe Exception que é uma classe Checked, que precisa ser
-//informada na assnatura do metodo ao qual sera utilizada
+//classe que contem o nome da minha exceção personalizada, estendendo a classe Exception que é uma classe Checked, que precisa ser
+//informada na assinatura do metodo ao qual sera utilizada
 public class CadastroException extends Exception {
 	
 //	contrutor da minha classe de exceção personalizada que recebe uma String no momento da sua criação e envia para a super classe
